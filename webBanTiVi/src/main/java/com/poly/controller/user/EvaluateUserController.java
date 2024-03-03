f1ad7e061c0672ccdf3603ece9dd8d487652c701
PhamDucOanh <114309807+oanhpdph@users.noreply.github.com>
2024-03-03 14:15:15 +0700

    Oanh Commit

--- webBanTiVi/src/main/java/com/poly/controller/user/EvaluateUserController.java
+++ webBanTiVi/src/main/java/com/poly/controller/user/EvaluateUserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
-public class EvaluateController {
+public class EvaluateUserController {
 
     @Autowired
     private EvaluateService evaluateService;
